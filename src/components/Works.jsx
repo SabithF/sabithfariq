df7e7a4ac08c4051fc2ff8d589cda1a6cf30c495
Sabithf <sabithfariq@gmail.com>
2025-02-26 22:43:21 +0000

    Work cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,10 +5,11 @@ import { fadeIn, textVariant } from "../utils/motion"
 import {motion} from 'framer-motion';
 import {styles} from '../styles'
 import { Tilt } from "react-tilt";
+import React from 'react';
 // import { scale } from "maath/dist/declarations/src/vector2";
 
 
-const ProjectCard = (index, name , description, tags, image , source_code_link ) => {
+const ProjectCard = ({index, name , description, tags, image , source_code_link} ) => {
   return (
     <motion.div variants={fadeIn("up","spring", index * 0.5,0.75)}> 
       <Tilt
@@ -18,6 +19,27 @@ const ProjectCard = (index, name , description, tags, image , source_code_link )
           speed: 450}}
           className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
           >
+
+          <div className="realtive w-full h-[230px]">
+            <img 
+              src={image} 
+              alt={name} 
+              className="w-full h-full object-cover rounded-2xl"
+               />
+          <div className="absolute inset-0 flex justify-end
+          m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center
+              items-center cursor-pointer">
+
+              </div>
+
+          </div>
+
+
+
+          </div>
 
       </Tilt>
     </motion.div>
