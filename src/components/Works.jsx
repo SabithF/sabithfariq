1b8939ba06b4fe1c9853fa472baf79f1c92fd4c7
Sabithf <sabithfariq@gmail.com>
2025-04-02 00:34:10 +0100

    Full site with them contact form function

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -33,6 +33,11 @@ const ProjectCard = ({index, name , description, tags, image , source_code_link}
               className="black-gradient w-10 h-10 rounded-full flex justify-center
               items-center cursor-pointer">
 
+                <img 
+                  src={github} 
+                  alt="github link"
+                  className="w-1/2 h-1/2 object-contain " />
+
               </div>
 
           </div>
@@ -40,7 +45,23 @@ const ProjectCard = ({index, name , description, tags, image , source_code_link}
 
 
           </div>
+          
+          <div className="mt-5">
+            <h3 className="text-white font-bold text-[24px]">
+              {name}
+            </h3>
+            <p className="mt-2 text-secondary text-[14px]">
+              {description}
+            </p>
+          </div>
+          <div className="mt-4 flex felx-wrap gap-2">
+            {tags.map((tag) => (
+              <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+                #{tag.name}
+              </p>
 
+            ))}
+          </div>
       </Tilt>
     </motion.div>
     
